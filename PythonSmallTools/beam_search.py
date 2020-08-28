7f3ad2f34b03da60d097919a8256940114d5870b
xiaobuguilaile <m18801919240_3@163.com>
2020-08-28 15:51:49 +0800

    add 1-beam_search 2-file_split 3-iterator

---
+++ PythonSmallTools/beam_search.py
@@ -0,0 +1,8 @@
+# -*-coding:utf-8 -*-
+
+'''
+@File       : beam_search.py
+@Author     : HW Shen
+@Date       : 2020/8/12
+@Desc       :
+'''
---
+++ PythonSmallTools/iterator.py
@@ -0,0 +1,8 @@
+# -*-coding:utf-8 -*-
+
+'''
+@File       : iterator.py
+@Author     : HW Shen
+@Date       : 2020/8/28
+@Desc       :
+'''
